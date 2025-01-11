e48efef210b42d011d9b2fc1801a20d8db283db6
Hamees123 <113780509+Hamees123@users.noreply.github.com>
2025-01-12 00:17:34 +0500

    Update Strings.py

--- Lab Task 1/Strings.py
+++ Lab Task 1/Strings.py
@@ -1,9 +1,9 @@
 s='Pakistan' #string
 
-print(s.capitalize()) #Output Hammas
-print(s.upper()) #Output HAMMAS
-print(s.lower()) #Output hammas
-print(s.find('a')) #Output 2
-print(s.replace('a','b')) #Output Hbmmbs
+print(s.capitalize()) #Output Pakistan
+print(s.upper()) #Output PAKISTAN
+print(s.lower()) #Output pakistan
+print(s.find('a')) #Output 1
+print(s.replace('a','b')) #Output Pbkistbn
 print(s.startswith('a')) #Output False
-print(s.split('s'))  # Output: ['Hamma', '']
+print(s.split('s'))  # Output: ['Paki', 'tan']
